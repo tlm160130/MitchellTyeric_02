930799370b7e9578d49264e519fca22fb7d8d4c1
Tyeric Mitchell <tlm160130@utdallas.edu>
2020-10-03 02:33:35 -0500

    PlayerMovement/Gravity/Muzzle Done

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public ParticleSystem muzzleFlash;
+
+    [SerializeField] 
+    private AudioSource shootSound;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        shootSound.Play();
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 6f;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public float jumpHeight = 3f;
+    public float sprint = 60f;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //checks if we've hit the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y< 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z; //Moves based on x and z movement and where the player is facing
+        //don't want to use new Vector3() because those are GLOBAL Coordinates
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            controller.Move(move * sprint * Time.deltaTime);
+        } 
+
+        //we use Velocity to handle Gravity
+        velocity.y += gravity * Time.deltaTime;
+
+        //physics of a free fall
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
